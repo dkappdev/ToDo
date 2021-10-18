4ea3dc4ddaf3e5758f849e6fa174c5476b7f0d14
Daniil <dkappdev@icloud.com>
2021-10-18 11:48:27 +0300

    Fix reference cycle in ToDoGroupList module

--- ToDo/Modules/ToDoGroupList/Presenters/ToDoGroupListPresenter.swift
+++ ToDo/Modules/ToDoGroupList/Presenters/ToDoGroupListPresenter.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public class ToDoGroupListPresenter: AnyToDoGroupListPresenter {
-    public var view: AnyToDoGroupListView?
+    public weak var view: AnyToDoGroupListView?
     public var interactor: AnyToDoGroupListInteractorInput?
     public var wireframe: AnyToDoGroupListWireframe?
     
